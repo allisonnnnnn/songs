881a1a8bee8a4cb5407c56cbb0709edde0be380a
allison <allisonchen1130@gmail.com>
2020-02-18 23:25:43 -0500

    Configured connect with mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -8,4 +8,8 @@ class SongList extends React.Component {
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
